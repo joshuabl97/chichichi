8e1f27dcc8f341fcc90668f62638d88959b8a526
joshuabl97 <blau.joshua@gmail.com>
2023-07-07 23:51:37 -0400

    fixed middleware.go to include reroute handler

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,22 +5,40 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
+
+type statusLoggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *statusLoggingResponseWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
+}
 
 func NewMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
-		writer := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		writer := &statusLoggingResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
 
 		next.ServeHTTP(writer, r)
 
 		latency := time.Since(start)
 
-		statusCode := writer.Status()
+		statusCode := writer.statusCode
 		statusText := http.StatusText(statusCode)
 
 		fmt.Printf("%s %s %s %s\n", r.Method, r.RequestURI, statusText, latency)
 	})
+}
+
+func Logger(handler http.Handler) http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Mount("/", handler)
+	return r
 }
